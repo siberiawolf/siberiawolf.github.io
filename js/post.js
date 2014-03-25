e67097b2676a6a5747d10d234e628509a13d6824
siberiawolf <yuxiangliang@3dly.net>
2014-03-25 15:15:57 +0800

    添加我自己的disqus代码，测试动态加载评论

--- js/post.js
+++ js/post.js
@@ -23,16 +23,12 @@ $(document).ready(function(){
 
     $('pre').addClass('prettyprint linenums'); //添加Google code Hight需要的class
 
-    //***********************
-    //**评论的代码也删掉哦***
-    // window.disqus_shortname = 'beiyuu'; // required: replace example with your forum shortname
-    // $('#disqus_container .comment').on('click',function(){
-    //     $(this).html('加载中...');
-    //     var that = this;
-    //     $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
-    // });
-    //**评论的代码也删掉哦***
-    //***********************
+    window.disqus_shortname = 'siberiawolf'; // required: replace example with your forum shortname
+    $('#disqus_container .comment').on('click',function(){
+        $(this).html('加载中...');
+        var that = this;
+        $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
+    });
 
     $('.entry a').each(function(index,element){
         var href = $(this).attr('href');
