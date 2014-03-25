72309e6c42c0758fe5cdb4729f06ffea2e80ab13
siberiawolf <yuxiangliang@3dly.net>
2014-03-25 14:59:45 +0800

    删掉原来post中的disqus

--- js/post.js
+++ js/post.js
@@ -25,12 +25,12 @@ $(document).ready(function(){
 
     //***********************
     //**评论的代码也删掉哦***
-    window.disqus_shortname = 'beiyuu'; // required: replace example with your forum shortname
-    $('#disqus_container .comment').on('click',function(){
-        $(this).html('加载中...');
-        var that = this;
-        $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
-    });
+    // window.disqus_shortname = 'beiyuu'; // required: replace example with your forum shortname
+    // $('#disqus_container .comment').on('click',function(){
+    //     $(this).html('加载中...');
+    //     var that = this;
+    //     $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
+    // });
     //**评论的代码也删掉哦***
     //***********************
 
