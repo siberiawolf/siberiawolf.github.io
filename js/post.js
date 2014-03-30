38ff1734af2fa58b5d90c49da5410839b7fc28f0
siberiawolf <yuxiangliang@3dly.net>
2014-03-30 17:36:16 +0800

    测试使用多说评论

--- js/post.js
+++ js/post.js
@@ -23,12 +23,12 @@ $(document).ready(function(){
 
     $('pre').addClass('prettyprint linenums'); //添加Google code Hight需要的class
 
-    window.disqus_shortname = 'siberiawolf'; // required: replace example with your forum shortname
-    $('#disqus_container .comment').on('click',function(){
-        $(this).html('加载中...');
-        var that = this;
-        $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
-    });
+    // window.disqus_shortname = 'siberiawolf'; // required: replace example with your forum shortname
+    // $('#disqus_container .comment').on('click',function(){
+    //     $(this).html('加载中...');
+    //     var that = this;
+    //     $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
+    // });
 
     $('.entry a').each(function(index,element){
         var href = $(this).attr('href');
